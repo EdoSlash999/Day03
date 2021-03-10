11c420d507a2ec4204ffb74b2d2d1d74eba7338a
EdoSlash999 <edo_tz_@hotmail.com>
2021-03-10 15:45:33 +0100

    zavrsen zdatak

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne= 15, numTwo = 8) {
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var diference=numOne-numTwo;
+    // Only change code above this line
+    return diference;
+   
+}
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
+module.exports = main;
