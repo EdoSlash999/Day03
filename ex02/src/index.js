cd1567f12d4d067b25e9137d0e7ec8d547946794
EdoSlash999 <edo_tz_@hotmail.com>
2021-03-10 12:51:58 +0100

    rijeseni zadaci

---
+++ ex02/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+    // prvi_komentar
+    
+    /* document.write("This is a multi-line comment!"); 
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!"); 
+    */
+    // document.write("This is a single-line comment!"); 
+    // document.write("This is a single-line comment!");
+    // document.write("This is a single-line comment!");
+    // document.write("This is a single-line comment!");
+    // document.write("This is a single-line comment!"); 
+
+
+    // Only change code above this line
+}
+main();
+module.exports = main;
---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var x = 7;
+    var y = 12;
+    var z = "My name is ";
+    // Only change code above this line
+    return[x, y, z];
+}
+main();
+module.exports = main;
